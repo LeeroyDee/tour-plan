05a48f2760f8cfdc222db35be42e003aaccf94f7
LeeroyDee <59417798+LeeroyDee@users.noreply.github.com>
2021-07-12 21:43:45 +0300

    create: slider

---
+++ js/main.js
@@ -0,0 +1,18 @@
+var swiper = new Swiper('.swiper-container', {
+  // Optional parameters
+  loop: true,
+
+
+  // Navigation arrows
+  navigation: {
+    nextEl: '.slider-button--next',
+    prevEl: '.slider-button--prev',
+  },
+
+});
+
+//swiper.slideNext();
+// document.addEventListener("keydown", (e) =>{
+//   if(e.keycode===39) swiper.slideNext();
+//   if(e.keycode===37) swiper.slidePref();
+// })
