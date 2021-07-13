ef4c7fde766a12583185636c774e5c52821a2c7c
LeeroyDee <59417798+LeeroyDee@users.noreply.github.com>
2021-07-13 21:45:53 +0300

    create: add JavaScript API

--- js/main.js
+++ js/main.js
@@ -19,3 +19,59 @@ var swiper = new Swiper('.swiper-container', {
 //   if(e.keycode===39) swiper.slideNext();
 //   if(e.keycode===37) swiper.slidePref();
 // })
+
+
+ymaps.ready(function () {
+  var myMap = new ymaps.Map('map', {
+          center: [55.7763358,37.5848271],
+          zoom: 14
+      }, {
+          searchControlProvider: 'yandex#search'
+      }),
+
+      // Создаём макет содержимого.
+      MyIconContentLayout = ymaps.templateLayoutFactory.createClass(
+          '<div style="color: #000000; font-size:18px; font-weight: bold;">$[properties.iconContent]</div>'
+      ),
+
+      myPlacemark = new ymaps.Placemark(myMap.getCenter(), {
+          hintContent: 'Отель Марриотт Москва Тверская',
+          balloonContent: 'Отель Марриотт Москва Тверская'
+      }, {
+          // Опции.
+          // Необходимо указать данный тип макета.
+          iconLayout: 'default#image',
+          // Своё изображение иконки метки.
+          iconImageHref: '',
+          // Размеры метки.
+          iconImageSize: [20, 50],
+          // Смещение левого верхнего угла иконки относительно
+          // её "ножки" (точки привязки).
+          iconImageOffset: [-5, -38]
+      }),
+
+      myPlacemarkWithContent = new ymaps.Placemark([55.7763358,37.5848271], {
+          hintContent: 'Отель Марриотт Москва Тверская',
+          balloonContent: 'Отель Марриотт Москва Тверская',
+          iconContent: 'Марриотт'
+      }, {
+          // Опции.
+          // Необходимо указать данный тип макета.
+          iconLayout: 'default#imageWithContent',
+          // Своё изображение иконки метки.
+          iconImageHref: 'img/iconMap.svg',
+          // Размеры метки.
+          iconImageSize: [28, 48],
+          // Смещение левого верхнего угла иконки относительно
+          // её "ножки" (точки привязки).
+          iconImageOffset: [-24, -24],
+          // Смещение слоя с содержимым относительно слоя с картинкой.
+          iconContentOffset: [30, -10],
+          // Макет содержимого.
+          iconContentLayout: MyIconContentLayout
+      });
+
+  myMap.geoObjects
+      .add(myPlacemark)
+      .add(myPlacemarkWithContent);
+});
