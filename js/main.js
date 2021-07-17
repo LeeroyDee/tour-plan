a6f0b820c5f4733b3aaccc07b1303718e505a9d3
LeeroyDee <59417798+LeeroyDee@users.noreply.github.com>
2021-07-17 20:21:08 +0300

    fix: index

--- js/main.js
+++ js/main.js
@@ -1,94 +1,96 @@
-var hotelSlider = new Swiper('.hotel-slider', {
-  // Optional parameters
-  loop: true,
 
-
-  // Navigation arrows
-  navigation: {
-    nextEl: '.hotel-slider__button--next',
-    prevEl: '.hotel-slider__button--prev',
-  },
-  keyboard: {
-    enabled: true,
-  },
-
-});
-var reviewsSlider = new Swiper('.reviews-slider', {
+  var hotelSlider = new Swiper('.hotel-slider', {
     // Optional parameters
     loop: true,
   
   
     // Navigation arrows
     navigation: {
-      nextEl: '.reviews-slider__button--next',
-      prevEl: '.reviews-slider__button--prev',
+      nextEl: '.hotel-slider__button--next',
+      prevEl: '.hotel-slider__button--prev',
     },
     keyboard: {
       enabled: true,
     },
   
   });
+  var reviewsSlider = new Swiper('.reviews-slider', {
+      // Optional parameters
+      loop: true,
+    
+    
+      // Navigation arrows
+      navigation: {
+        nextEl: '.reviews-slider__button--next',
+        prevEl: '.reviews-slider__button--prev',
+      },
+      keyboard: {
+        enabled: true,
+      },
+    
+    });
+  
+  //swiper.slideNext();
+  // document.addEventListener("keydown", (e) =>{
+  //   if(e.keycode===39) swiper.slideNext();
+  //   if(e.keycode===37) swiper.slidePref();
+  // })
+  
+  
+  ymaps.ready(function () {
+    var myMap = new ymaps.Map('map', {
+            center: [55.7763358,37.5848271],
+            zoom: 14
+        }, {
+            searchControlProvider: 'yandex#search'
+        }),
+  
+        // Создаём макет содержимого.
+        MyIconContentLayout = ymaps.templateLayoutFactory.createClass(
+            '<div style="color: #000000; font-size:18px; font-weight: bold;">$[properties.iconContent]</div>'
+        ),
+  
+        myPlacemark = new ymaps.Placemark(myMap.getCenter(), {
+            hintContent: 'Отель Марриотт Москва Тверская',
+            balloonContent: 'Отель Марриотт Москва Тверская'
+        }, {
+            // Опции.
+            // Необходимо указать данный тип макета.
+            iconLayout: 'default#image',
+            // Своё изображение иконки метки.
+            iconImageHref: '',
+            // Размеры метки.
+            iconImageSize: [20, 50],
+            // Смещение левого верхнего угла иконки относительно
+            // её "ножки" (точки привязки).
+            iconImageOffset: [-5, -38]
+        }),
+  
+        myPlacemarkWithContent = new ymaps.Placemark([55.7763358,37.5848271], {
+            hintContent: 'Отель Марриотт Москва Тверская',
+            balloonContent: 'Отель Марриотт Москва Тверская',
+            iconContent: 'Марриотт'
+        }, {
+            // Опции.
+            // Необходимо указать данный тип макета.
+            iconLayout: 'default#imageWithContent',
+            // Своё изображение иконки метки.
+            iconImageHref: 'img/iconMap.svg',
+            // Размеры метки.
+            iconImageSize: [28, 48],
+            // Смещение левого верхнего угла иконки относительно
+            // её "ножки" (точки привязки).
+            iconImageOffset: [-24, -24],
+            // Смещение слоя с содержимым относительно слоя с картинкой.
+            iconContentOffset: [30, -10],
+            // Макет содержимого.
+            iconContentLayout: MyIconContentLayout
+        });
+  
+    myMap.geoObjects
+        .add(myPlacemark)
+        .add(myPlacemarkWithContent);
+  });
+  
+  $('newsletter').parallax({imageSrc: '../img/newsletter-bg.jpeg'});
 
-//swiper.slideNext();
-// document.addEventListener("keydown", (e) =>{
-//   if(e.keycode===39) swiper.slideNext();
-//   if(e.keycode===37) swiper.slidePref();
-// })
-
-
-ymaps.ready(function () {
-  var myMap = new ymaps.Map('map', {
-          center: [55.7763358,37.5848271],
-          zoom: 14
-      }, {
-          searchControlProvider: 'yandex#search'
-      }),
-
-      // Создаём макет содержимого.
-      MyIconContentLayout = ymaps.templateLayoutFactory.createClass(
-          '<div style="color: #000000; font-size:18px; font-weight: bold;">$[properties.iconContent]</div>'
-      ),
-
-      myPlacemark = new ymaps.Placemark(myMap.getCenter(), {
-          hintContent: 'Отель Марриотт Москва Тверская',
-          balloonContent: 'Отель Марриотт Москва Тверская'
-      }, {
-          // Опции.
-          // Необходимо указать данный тип макета.
-          iconLayout: 'default#image',
-          // Своё изображение иконки метки.
-          iconImageHref: '',
-          // Размеры метки.
-          iconImageSize: [20, 50],
-          // Смещение левого верхнего угла иконки относительно
-          // её "ножки" (точки привязки).
-          iconImageOffset: [-5, -38]
-      }),
-
-      myPlacemarkWithContent = new ymaps.Placemark([55.7763358,37.5848271], {
-          hintContent: 'Отель Марриотт Москва Тверская',
-          balloonContent: 'Отель Марриотт Москва Тверская',
-          iconContent: 'Марриотт'
-      }, {
-          // Опции.
-          // Необходимо указать данный тип макета.
-          iconLayout: 'default#imageWithContent',
-          // Своё изображение иконки метки.
-          iconImageHref: 'img/iconMap.svg',
-          // Размеры метки.
-          iconImageSize: [28, 48],
-          // Смещение левого верхнего угла иконки относительно
-          // её "ножки" (точки привязки).
-          iconImageOffset: [-24, -24],
-          // Смещение слоя с содержимым относительно слоя с картинкой.
-          iconContentOffset: [30, -10],
-          // Макет содержимого.
-          iconContentLayout: MyIconContentLayout
-      });
-
-  myMap.geoObjects
-      .add(myPlacemark)
-      .add(myPlacemarkWithContent);
-});
-
-$('newsletter').parallax({imageSrc: '../img/newsletter-bg.jpeg'});
