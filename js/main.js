59c0645259d3a6f5a60a7ec12af1e8d7a33f0b03
LeeroyDee <59417798+LeeroyDee@users.noreply.github.com>
2021-07-27 20:53:03 +0300

    fix: responsive

--- js/main.js
+++ js/main.js
@@ -1,3 +1,61 @@
+
+//яндекс карта
+ymaps.ready(function () {
+  var myMap = new ymaps.Map('map', {
+          center: [55.7763358,37.5848271],
+          zoom: 14
+      }, {
+          searchControlProvider: 'yandex#search'
+      }),
+
+      // Создаём макет содержимого.
+      MyIconContentLayout = ymaps.templateLayoutFactory.createClass(
+          '<div style="color: #000000; font-size:18px; font-weight: bold;">$[properties.iconContent]</div>'
+      ),
+
+      myPlacemark = new ymaps.Placemark(myMap.getCenter(), {
+          hintContent: 'Отель Марриотт Москва Тверская',
+          balloonContent: 'Отель Марриотт Москва Тверская'
+      }, {
+          // Опции.
+          // Необходимо указать данный тип макета.
+          iconLayout: 'default#image',
+          // Своё изображение иконки метки.
+          iconImageHref: '',
+          // Размеры метки.
+          iconImageSize: [20, 50],
+          // Смещение левого верхнего угла иконки относительно
+          // её "ножки" (точки привязки).
+          iconImageOffset: [-5, -38]
+      }),
+
+      myPlacemarkWithContent = new ymaps.Placemark([55.7763358,37.5848271], {
+          hintContent: 'Отель Марриотт Москва Тверская',
+          balloonContent: 'Отель Марриотт Москва Тверская',
+          iconContent: 'Марриотт'
+      }, {
+          // Опции.
+          // Необходимо указать данный тип макета.
+          iconLayout: 'default#imageWithContent',
+          // Своё изображение иконки метки.
+          iconImageHref: 'img/iconMap.svg',
+          // Размеры метки.
+          iconImageSize: [28, 48],
+          // Смещение левого верхнего угла иконки относительно
+          // её "ножки" (точки привязки).
+          iconImageOffset: [-24, -24],
+          // Смещение слоя с содержимым относительно слоя с картинкой.
+          iconContentOffset: [30, -10],
+          // Макет содержимого.
+          iconContentLayout: MyIconContentLayout
+      });
+
+  myMap.geoObjects
+      .add(myPlacemark)
+      .add(myPlacemarkWithContent);
+});
+
+
 $(document).ready(function(){
   var hotelSlider = new Swiper('.hotel-slider', {
     // Optional parameters
@@ -30,67 +88,9 @@ $(document).ready(function(){
     
     });
   
-  //swiper.slideNext();
-  // document.addEventListener("keydown", (e) =>{
-  //   if(e.keycode===39) swiper.slideNext();
-  //   if(e.keycode===37) swiper.slidePref();
-  // })
+ 
   
-  //яндекс карта
-  ymaps.ready(function () {
-    var myMap = new ymaps.Map('map', {
-            center: [55.7763358,37.5848271],
-            zoom: 14
-        }, {
-            searchControlProvider: 'yandex#search'
-        }),
   
-        // Создаём макет содержимого.
-        MyIconContentLayout = ymaps.templateLayoutFactory.createClass(
-            '<div style="color: #000000; font-size:18px; font-weight: bold;">$[properties.iconContent]</div>'
-        ),
-  
-        myPlacemark = new ymaps.Placemark(myMap.getCenter(), {
-            hintContent: 'Отель Марриотт Москва Тверская',
-            balloonContent: 'Отель Марриотт Москва Тверская'
-        }, {
-            // Опции.
-            // Необходимо указать данный тип макета.
-            iconLayout: 'default#image',
-            // Своё изображение иконки метки.
-            iconImageHref: '',
-            // Размеры метки.
-            iconImageSize: [20, 50],
-            // Смещение левого верхнего угла иконки относительно
-            // её "ножки" (точки привязки).
-            iconImageOffset: [-5, -38]
-        }),
-  
-        myPlacemarkWithContent = new ymaps.Placemark([55.7763358,37.5848271], {
-            hintContent: 'Отель Марриотт Москва Тверская',
-            balloonContent: 'Отель Марриотт Москва Тверская',
-            iconContent: 'Марриотт'
-        }, {
-            // Опции.
-            // Необходимо указать данный тип макета.
-            iconLayout: 'default#imageWithContent',
-            // Своё изображение иконки метки.
-            iconImageHref: '../img/iconMap.svg',
-            // Размеры метки.
-            iconImageSize: [28, 48],
-            // Смещение левого верхнего угла иконки относительно
-            // её "ножки" (точки привязки).
-            iconImageOffset: [-24, -24],
-            // Смещение слоя с содержимым относительно слоя с картинкой.
-            iconContentOffset: [30, -10],
-            // Макет содержимого.
-            iconContentLayout: MyIconContentLayout
-        });
-  
-    myMap.geoObjects
-        .add(myPlacemark)
-        .add(myPlacemarkWithContent);
-  });
 
 let menuButton=document.querySelector(".menu-button")
 menuButton.addEventListener("click",function (){
@@ -143,6 +143,12 @@ menuButton.addEventListener("click",function (){
   } );
   //маска
   $('.phone_us').mask('+7(000) 000-00-00');
+  AOS.init({
+    disable: function () {
+      var maxWidth = 768;
+      return window.innerWidth < maxWidth;
+    }
+  });
 });
 
 
